9be2e5f20d4f9731d49daa0263168f3bd115436c
Devyani <devyani@alumni.stanford.edu>
2025-08-16 17:17:18 -0700

    Update test_makeStep

--- functions/MakeStep/test_makeStep.m
+++ functions/MakeStep/test_makeStep.m
@@ -1,4 +1,3 @@
-clear
 import matlab.unittest.TestCase
 import matlab.unittest.constraints.IsEqualTo
 import matlab.unittest.constraints.AbsoluteTolerance
@@ -8,30 +7,32 @@ testCase = TestCase.forInteractiveUse;
 
 for test_idx=1:2
     for test_type=["U", "F"]
-    data = load("MakeStepTestJacobian_"+test_type+"_"+num2str(test_idx)+".mat");
-    p = data.p; 
-    v = data.v;
-    a = data.a;
-    varargin = data.varargin{1};
-    expected_p = data.p_new;
-    expected_v = data.v_new;
-    expected_E_v = data.E_v;
-    labels = data.labels;
-    beta = data.beta;
-    i = data.i;
-    mass = data.mass;
-    dt = data.dt;
-    if test_type == "U"
-        getJacobian = @getJacobian_u;
-        getb = @getb_u;
-    else
-        getJacobian = @getJacobian_f;
-        getb = @getb_f;
-    end
 
-    [actual_p, actual_v, actual_E_v] = makeStep(p, v, a, getJacobian, getb, labels, beta, i, mass, dt, varargin);
-    testCase.verifyThat(actual_p,IsEqualTo(expected_p, "Within",AbsoluteTolerance(eps)))
-    testCase.verifyThat(actual_v,IsEqualTo(expected_v, "Within",AbsoluteTolerance(eps)))
-    testCase.verifyThat(actual_E_v,IsEqualTo(expected_E_v, "Within",AbsoluteTolerance(eps)))
+        data = load("MakeStepTestJacobian_"+test_type+"_"+num2str(test_idx)+".mat");
+        p = data.p;
+        v = data.v;
+        a = data.a;
+        varargin = data.varargin{1};
+        expected_p = data.p_new;
+        expected_v = data.v_new;
+        expected_E_v = data.E_v;
+        labels = data.labels;
+        beta = data.beta;
+        i = data.i;
+        mass = data.mass;
+        dt = data.dt;
+        if test_type == "U"
+            getJacobian = @getJacobian_u;
+            getb = @getb_u;
+        else
+            getJacobian = @getJacobian_f;
+            getb = @getb_f;
+        end
+
+        [actual_p, actual_v, actual_E_v] = makeStep(p, v, a, getJacobian, getb, labels, beta, i, mass, dt, varargin);
+        testCase.verifyThat(actual_p,IsEqualTo(expected_p, "Within",AbsoluteTolerance(eps)));
+        testCase.verifyThat(actual_v,IsEqualTo(expected_v, "Within",AbsoluteTolerance(eps)));
+        testCase.verifyThat(actual_E_v,IsEqualTo(expected_E_v, "Within",AbsoluteTolerance(eps)));
+
     end
 end
